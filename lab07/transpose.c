b02f65f4f0724a2b74f1f9e0d364640401899693
Huiming <610416981@qq.com>
2022-12-13 09:11:25 +0800

    done with lab07

--- lab07/transpose.c
+++ lab07/transpose.c
@@ -13,4 +13,25 @@ void transpose_naive(int n, int blocksize, int *dst, int *src) {
  * multiple of the block size. */
 void transpose_blocking(int n, int blocksize, int *dst, int *src) {
     // YOUR CODE HERE
+    int chunk_num = n / blocksize;
+
+    // normal case
+    for (int i = 0; i < chunk_num; i++) {
+        for (int j = 0; j < chunk_num; j++) {
+            for (int x = 0; x < blocksize; x++) {
+                for (int y = 0; y < blocksize; y++) {
+                    dst[y + j * blocksize + (x + i * blocksize) * n] = src[x + i * blocksize + (y + j * blocksize) * n];
+                }
+            }
+        }
+    }
+
+    // If n is not a multiple of blocksize, handle the edge case
+    int normalsize = chunk_num * blocksize;
+    for (int k = 0; k < n; k++) {
+        for (int z = normalsize; z < n; z++) {
+            dst[z + k * n] = src[k + z * n];
+            dst[k + z * n] = src[z + k * n];
+        }
+    } 
 }
